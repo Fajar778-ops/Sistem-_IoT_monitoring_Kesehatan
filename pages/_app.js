bc97eba6c99a3ecc2fd5c0c389f43532cb30c131
Fajar <chuckguy555@gmail.com>
2025-12-13 22:40:35 +0800

    Tambahan Loading Screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,41 @@
 import "@/styles/globals.css";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Timer 2 detik (2000ms)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // TAMPILAN LOADING SCREEN
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <div className="loader-content">
+          {/* Logo Kampus */}
+          <img 
+            src="/Logo-Resmi-Unhas-1" 
+            alt="Logo Universitas Hasanuddin" 
+            className="loader-logo" 
+          />
+          
+          {/* Bar Loading Keren */}
+          <div className="progress-bar-container">
+            <div className="progress-bar-fill"></div>
+          </div>
+          
+          <p className="loading-text">Memuat Sistem EKG...</p>
+        </div>
+      </div>
+    );
+  }
+
+  // TAMPILAN WEBSITE ASLI
   return <Component {...pageProps} />;
 }
